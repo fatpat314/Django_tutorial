0874eb780f29f9f1293c6a597cdb0557a495a06c
Patrick <w.patrick.kelly@gmail.com>
2020-02-11 17:10:33 -0800

    page five done

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -16,5 +22,5 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,8 @@ from django.views import generic
 # from django.template import loader
 # from django.shortcuts import render
 from .models import Choice, Question
+from django.utils import timezone
+
 
 
 class IndexView(generic.ListView):
@@ -16,11 +18,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
